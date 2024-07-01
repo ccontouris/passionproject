c91e010d6feef102fd8b9480d19dd6705f12d57e
Christi Contouris <ccontouris@icstars.org>
2024-07-01 08:33:23 -0500

    fixed footer

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ async function getBirdMigration() {
 }
 
 getBirdMigration()
+
+
+// $('#dove').hover(function(){
+//     PlaySound('"C:\Users\ChrisitContouris\OneDrive - i.c.stars\Documents\Sound Recordings\Mourning Dove.m4a"');// calling playsound function
+// },function(){
+//     StopSound('"C:\Users\ChrisitContouris\OneDrive - i.c.stars\Documents\Sound Recordings\Mourning Dove.m4a"');// calling stopsound function
+// });
