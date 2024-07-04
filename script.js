470f05d7c832faf4fc8ab509bf5a02cd6c014505
Christi Contouris <ccontouris@icstars.org>
2024-07-04 10:56:45 -0500

    finally figured out autoplay yaya!

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ async function getBirdMigration() {
         "Accept": "*/*",
         "x-ebirdapitoken": "tfdjg87simiq"
     }
-    
-    let response = await fetch("https://api.ebird.org/v2/data/obs/US-IL/recent", { 
+
+    let response = await fetch("https://api.ebird.org/v2/data/obs/US-IL/recent", {
         method: "GET",
         headers: headersList
     });
-    
+
     let data = await response.json();
     let firstBird = data[0]
     document.getElementById("Migration").innerHTML = firstBird.comName
     console.log(data);
-    
+
 
 }
 
 getBirdMigration()
+
+document.getElementById("BM").style.display = 'none';
 
 
 // $('#dove').hover(function(){
